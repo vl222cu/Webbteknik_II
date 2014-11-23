3c32cbe9f54ff6ac7e8ec09c41a709b20cc2f9b7
vl222cu <vl222cu@student.lnu.se>
2014-11-23 13:07:34 +0100

    Lägger om labb 2

--- Laboration_2/1DV449_L02/test/debug.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // This is just a stupid place where we imagine of debuging stuff is going on...
-    // This should only be called in development enviroment
-    $nanos = rand(100000000, 999999999);
-    time_nanosleep(1, $nanos);
-    header("Location: ../mess.php");
-?>
